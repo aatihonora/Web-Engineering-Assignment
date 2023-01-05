76785a8bb42b9d0f8d3f696e7b544860b0af180d
Behram Akhtar <58216695+aatihonora@users.noreply.github.com>
2023-01-04 23:21:02 -0800

    Add files via upload

---
+++ Shop-IT-Now/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Behram Akhtar Roll No. 2K19/CSM/17 | All Rights Reserved</b></span></div>
+</footer>
+    
